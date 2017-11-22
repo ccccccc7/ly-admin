f3eecaa5f039fb76926f13bb6d05352686d4d8f1
zyl <869937739@qq.com>
2017-11-22 15:17:55 +0800

    'update`

---
+++ src/components/user/Edit.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <el-form :model="form">
+      <el-form-item label="姓名" prop="name" :rules="[{ required: true,message: '姓名不能为空'}]">
+        <el-input type="name" v-model="form.name"></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        form: {}
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
--- src/components/user/List.vue
+++ src/components/user/List.vue
@@ -1,16 +1,24 @@
 <template>
   <div>
     <el-table :data="tableData" stripe style="width: 100%">
+      <el-table-column label="操作" width="100px">
+        <template slot-scope="scope">
+          <el-button type="text" size="small" @click="handleEdit(scope.$index,scope.row)">编辑</el-button>
+          <el-button type="text" size="small">删除</el-button>
+        </template>
+      </el-table-column>
       <el-table-column prop="date" label="日期"/>
       <el-table-column prop="name" label="姓名"/>
     </el-table>
-    <el-button plain @click="sucInfo">success</el-button>
-    <el-button plain @click="errInfo">error</el-button>
+    <div style="margin: 20px 0;">
+      <el-button plain type="primary" @click="addUser">新增</el-button>
+    </div>
   </div>
 </template>
 
 <script>
   import ajax from '../../util/ajax'
+  import edit from './Edit.vue'
   export default {
     components: ajax,
     data (){
@@ -33,6 +41,13 @@
           console.log(result)
         }, function () {
         })
+      },
+      addUser: function () {
+
+      },
+      handleEdit: function(index,row) {
+        console.log(index)
+        console.log(row)
       }
     }
   }
